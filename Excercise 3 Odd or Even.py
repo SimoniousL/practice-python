294be4e35412f7dafe6a2cef89a81661b139e2e7
SimoniousL <50794280+SimoniousL@users.noreply.github.com>
2026-08-09 12:05:45 +0200

    Delete Excercise 3 Odd or Even.py
    Wrong name, should be exercise 2.

--- Excercise 3 Odd or Even.py
+++
@@ -1,6 +0,0 @@
-input_number = int(input('Please enter a number: '))
-
-if input_number % 2 == 0:
-    print('This number is an even number.')
-elif input_number % 2 != 0:
-    print('This number is an odd number.')
